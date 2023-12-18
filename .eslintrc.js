5b6b3e3b97c8aaf65f6ed44bf093c74c041fa238
Clark Han <estylegg@gmail.com>
2023-12-18 17:50:38 +0900

    updated packages, eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,19 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    // parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
-    // 'prettier',
-    'prettier/vue'
-  ],
-  plugins: [
-    'prettier'
+    'prettier',
   ],
   // add your custom rules here
   rules: {
     "no-console": "off",
     "no-unused-vars": "off",
+    "nuxt/no-env-in-hooks": "off",
     "implicit-arrow-linebreak": "off",
     "vue/comment-directive": 0,
   }
